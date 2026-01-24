0269ddfd86e0581166c851ab96ce60e2f19a5953
joelmo <joelmofraga@gmail.com>
2026-01-24 10:26:53 -0400

    Commit inicial.

---
+++ src/main/java/com/joelmofraga/artists_albums_api/ArtistsAlbumsApiApplication.java
@@ -0,0 +1,13 @@
+package com.joelmofraga.artists_albums_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistsAlbumsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistsAlbumsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joelmofraga/artists_albums_api/ArtistsAlbumsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelmofraga.artists_albums_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistsAlbumsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
